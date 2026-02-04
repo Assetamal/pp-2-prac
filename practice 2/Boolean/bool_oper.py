79f7760a666037459471db0d886bb765fdadc9ff
Assetamal <a_amal@kbtu.kz>
2026-02-04 14:43:49 +0500

    added file

---
+++ practice 2/Boolean/bool_oper.py
@@ -0,0 +1,13 @@
+#1 ex
+a = 2
+b = 6
+print(a<b or b<=2)
+#2 ex
+a = 4
+b = 5
+print(a==b and a<b)
+print(a>b or a<5)
+#3 ex
+a = 44
+b = 23
+print(not(b<100 or a>b and a<=0))
---
+++ practice 2/loop/while_break.py
@@ -0,0 +1,13 @@
+# ex 1: break when reaching a point
+i = 1
+while i < 6:
+    print(i)
+    if i == 3:
+        break
+    i += 1
+# ex 2:
+a = 50 
+while a>0:
+    a-=1
+    print(a)
+    break
